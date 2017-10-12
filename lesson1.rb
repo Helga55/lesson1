c31c3a2f4eb06ced25686a5fbfd6d8d3c149037f
Helga Larsen <larsen0x4c@gmail.com>
2017-10-13 02:15:28 +0300

    Added methods

--- lesson1.rb
+++ lesson1.rb
@@ -1,14 +1,31 @@
 require 'date'
 class Lesson1
   def sum(val = 0)
-    # TODO
+    array = val.to_s.split('')
+
+    total_sum = 0
+    array.each do |a|
+      total_sum += a.to_i
+    end
+
+    total_sum
   end
 
   def age(birthday)
-    # TODO
+    birth = Date.parse(birthday)
+    days = (Date.today - birth).to_i
+
+    "I have lived #{days / 365} years or #{days} days or #{days * 24} hours or \
+    #{days * 24 * 60}  minutes or #{days * 24 * 60 * 60} seconds"
+  rescue ArgumentError, TypeError
+    'Invalid Date Format'
   end
 
   def name
-    # TODO
+    user_name       = gets.chomp
+    user_surname    = gets.chomp
+    user_patronymic = gets.chomp
+
+    "Hello #{user_name} #{user_surname} #{user_patronymic}!"
   end
 end
--- my_array.rb
+++ my_array.rb
@@ -6,58 +6,64 @@ class MyArray
   end
 
   def size
-    # TODO
+    @array.length
   end
 
   def reverse
-    # TODO
+    @array.reverse
   end
 
   def max
-    # TODO
+    @array.max
   end
 
   def min
-    # TODO
+    @array.min
   end
 
   def desc
-    # TODO
+    @array.sort.reverse
   end
 
   def asc
-    # TODO
+    @array.sort
   end
 
   def odd
-    # TODO
+    @array.select(&:odd?)
   end
 
   def multiple_to_three
-    # TODO
+    @array.select { |a| (a % 3).zero? }
   end
 
   def uniq
-    # TODO
+    @array.uniq
   end
 
   def devide_on_ten
-    # TODO
+    @array.map { |a| a / 10.0 }
   end
 
   def chars
-    # TODO
+    letters = @array.each.select { |a| a.between?(1, 26) }
+    letters.map { |a| (a + 'a'.ord - 1).chr.to_sym }
   end
 
   def switch
-    # TODO
+    max_i = @array.index(@array.max)
+    min_i = @array.index(@array.min)
+    @array[min_i], @array[max_i] = @array[max_i], @array[min_i]
+
+    @array
   end
 
   def before_min
-    # TODO
+    min = @array.min
+    @array.select { |a| a < min }
   end
 
   def three_smallest
-    # TODO
+    @array.sort.select { |a| a < 4 }
   end
 end
